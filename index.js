c8773986e50dae90aaa41a944dd604e2a102c0a7
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2023-01-11 14:24:16 +0200

    sub
    sub

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let first = document.getElementById("one")
 let second = document.getElementById("two")
 let count1 = 0
 let count2 =0 
+
+// first screen
 function add11(){
     count1 = 1 + count1
     first.innerText = count1
@@ -17,6 +19,21 @@ function add31(){
     first.innerText = count1
 }
 
+function sub11(){
+    count1 =  count1 -1
+    first.innerText = count1
+}
+
+function sub21(){
+    count1 =  count1 -2
+    first.innerText = count1
+}
+
+function sub31(){
+    count1 =   count1 -3
+    first.innerText = count1
+}
+//second screen
 function add12(){
     count2 = 1 + count2
     second.innerText = count2
@@ -29,5 +46,20 @@ function add22(){
 
 function add32(){
     count2 = 3 + count2
+    second.innerText = count2
+}
+
+function sub12(){
+    count2 =   count2 -1
+    second.innerText = count2
+}
+
+function sub22(){
+    count2 =  count2 - 2 
+    second.innerText = count2
+}
+
+function sub32(){
+    count2 =  count2 -3
     second.innerText = count2
 }
